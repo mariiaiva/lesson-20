5ca4e577ef2ba5d2978daaaad3cd7b1a7e4079e1
Mariia <mariia.ivakhnenko1@gmail.com>
2023-06-28 20:44:44 +0300

    added laptop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-var instances = M.Carousel.init(elems, {});
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
   });
